30568aae6f74929bc4510a503e9799af80db250a
TeguhEngineer <teguhsimdig8@gmail.com>
2025-09-25 13:08:00 +0700

    fix bug 1

---
+++ app/Http/Controllers/Api/ResetPasswordController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Services\WhatsAppService;
+use Illuminate\Http\Request;
+
+class ResetPasswordController extends Controller
+{
+    protected $wa;
+
+    public function __construct(WhatsAppService $wa)
+    {
+        $this->wa = $wa;
+    }
+
+    public function send(Request $request)
+    {
+        dd("ini api reset password");
+        $validated = $request->validate([
+            'phone'      => 'required|string',
+            'message' => 'required|string',
+        ]);
+
+        try {
+            $result = $this->wa->sendMessage($validated['phone'], $validated['message']);
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Pesan berhasil dikirim',
+                'data'    => $result
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'success' => false,
+                'error'   => $th->getMessage()
+            ], 500);
+        }
+    }
+}
